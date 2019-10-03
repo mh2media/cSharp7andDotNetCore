b26254330cca93dcd7a69a8f7509ebe1bfecceb5
mark_henry@questsys.com <mark_henry@questsys.com>
2019-10-03 13:08:59 -0700

    First Commit

---
+++ eBookManager/ImportBooks.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace eBookManager
+{
+    partial class ImportBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportBooks));
+            this.tvImages = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // tvImages
+            // 
+            this.tvImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("tvImages.ImageStream")));
+            this.tvImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.tvImages.Images.SetKeyName(0, "docx16.png");
+            this.tvImages.Images.SetKeyName(1, "docxx16.png");
+            this.tvImages.Images.SetKeyName(2, "epubx16.png");
+            this.tvImages.Images.SetKeyName(3, "folder_exp_x16.png");
+            this.tvImages.Images.SetKeyName(4, "folder-close-x16.png");
+            this.tvImages.Images.SetKeyName(5, "pdfx16.png");
+            // 
+            // ImportBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ImportBooks";
+            this.Text = "ImportBooks";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList tvImages;
+    }
+}
---
+++ eBookManager/eBookManager.Helper/ExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookManager.Helper
+{
+    public static class ExtensionMethods
+    {
+        public static double ToMegabytes(this long bytes)
+        {
+            return (bytes > 0) ? (bytes / 1024f) / 1024f : bytes;
+        }
+
+        public static int ToInt(this string value, int defaultInteger = 0)
+        {
+            try
+            {
+                if (int.TryParse(value, out int validInteger))
+                    // Out variables
+                    return validInteger;
+                else
+                    return defaultInteger;
+            }
+            catch
+            {
+                return defaultInteger;
+            }
+        }
+    }
+}
